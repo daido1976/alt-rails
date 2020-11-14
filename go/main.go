8cf1daf57e2e403585dba16191ef85ccb9213162
Daido Shota <daido1976@gmail.com>
2020-11-14 21:10:50 +0900

    Init todo api in go

--- go/main.go
+++ go/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+}
 
 func main() {
-	fmt.Println("hello")
+	server := http.Server{
+		Addr: ":8080",
+	}
+	http.HandleFunc("/todos", handleTodos)
+	log.Printf("start server localhost%s \n", server.Addr)
+	server.ListenAndServe()
+}
+
+func handleTodos(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Received %s %s", r.Method, r.URL)
+	todo1 := Todo{1, "hoge"}
+	todo2 := Todo{2, "fuga"}
+	todos := []Todo{todo1, todo2}
+
+	w.Header().Set("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", "  ")
+	err := encoder.Encode(&todos)
+	if err != nil {
+		return
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
